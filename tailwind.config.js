c79d70f00658810647794380d2e6468034636644
Mustafa Abada <mfadel98@gmail.com>
2021-07-12 19:45:59 +0300

    v1.0.3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
 module.exports = {
-  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      screen: {
-        "3xl": "2000px",
-      },
     },
   },
   variants: {
     extend: {},
-    animation: ["responsive", "motion-safe", "motion-reduce"],
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
