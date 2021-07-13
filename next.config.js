321598c7723abab545682417fe745c3ced45e2aa
Mustafa Abada <mfadel98@gmail.com>
2021-07-13 19:31:10 +0300

    now

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: 'https://hulu-bay.vercel.app/',
+      },
+    ]
+  },
   images: {
     domains: ['links.papareact.com' , 'image.tmdb.org'] 
   }
